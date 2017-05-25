102509a69d6509bd19fbed84e78c94401d8d64c9
Ty Tia <tytia77@gmail.com>
2017-05-25 20:50:26 +1000

    added logic

--- script.js
+++ script.js
@@ -1,129 +1,155 @@
 var app = angular.module('app', []);
     app.controller('ctrl', ['$scope', '$timeout', function($scope, $timeout){
 
-        var pattern = [];
-        $scope.display = '';
-        var userRecord = [];
-        var allowClick = true;
-        var begin = false;
         var wav =[
             'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',
             'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
             'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
             'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'
         ];
-        var test;
 
+        var gameState = {
+            allowClick: false,
+            onSwitch: false,
+            strictMode: false,
+            pattern: [],
+            userRecord: [],
+            timeout: {}
+        }
 
+        $scope.buttonTap = {
+            strict: function(){
+                if (gameState.onSwitch){
+                    $('.strict-led').toggleClass('light-green');
+                    gameState.strictMode = !gameState.strictMode;
+                }
+            },
 
-        // handle clicks
-        $('.tab').on('click', function(){
+            start: function(){
+                if (gameState.onSwitch){
+                    restartGame();
+                    initPlay();
+                }
+            },
 
-            if (allowClick){
-                var index = $(this).index();
-                toggleClass(index);
+            on: function(){
+                gameState.onSwitch = !gameState.onSwitch;
+                $('.button-container').toggleClass('on');
+                $('.fa').toggleClass('icon-green');
+
+                $scope.display = gameState.onSwitch ? '--' : '';
+
+                if (!gameState.onSwitch){
+                    clearTimer();
+                    $scope.display = '';
+                    gameState.strictMode = false;
+                    $('.strict-led').removeClass('light-green');
+                }
             }
+        }
 
-            if (begin){
-                userRecord.push(index);
-                checkUserClick();
+        $('.tab').on('click', function(){
+            var index;
+            if (gameState.allowClick){
+                index = $(this).index();
+                toggleClass(index);
+                gameState.userRecord.push(index);
+                checkPattern();
             }
         })
 
-        function checkUserClick(){
-            if(test){
-                $timeout.cancel(test);
-            }
 
-            test = $timeout(function(){
-                checkPattern();
-            },3000);
-
-            if(begin && userRecord.length >= pattern.length){
-                $timeout.cancel(test);
-                checkPattern();
-            }
-        }
-
-        $scope.buttonClick = function(){
-
-            begin = !begin;
-            $('.button-container').toggleClass('on');
-            $('.fa').toggleClass('icon-green');
-            var msg = begin ? 'click to stop' : 'click to start';
-            $('.button-container').attr('data-msg', msg);
-            play(begin);
-        }
-
-        function play(begin){
-            if (begin){
-                $scope.display = 1;
-                allowClick = false;
-                generatePattern();
-                playPattern(0);
-            } else {
-                $scope.display = '';
-                userRecord = [];
-                pattern = [];
-            }
+        // initialize game, first time
+        function initPlay(){
+            generatePattern();
+            playPattern(0);
         }
 
         function checkPattern(){
-            var match = true;
+            var user = gameState.userRecord;
+            var pattern = gameState.pattern;
+            var userLen = user.length - 1;
+            var patternLen = pattern.length - 1;
 
-            pattern.forEach(function(item, index){
-                if (item !== userRecord[index]){
-                    console.log('error');
-                    match = false;
+            if (user[userLen] === pattern[userLen]){
+
+                if (userLen === patternLen){
+
+                    // player achieved 20 correct
+                    if (userLen > 19){
+                        $scope.display = '**';
+                        $timeout(function(){
+                            $scope.buttonTap.start();
+                        }, 3000);
+                    } else {
+                        gameState.userRecord = [];
+                        $scope.display++;
+                        generatePattern();
+                        playPattern(0);
+                    }
                 }
-            })
 
-            if (match){
-                userRecord = [];
-                $scope.display++;
-                generatePattern();
-                playPattern(0);
-            } else {
+            }  else {
+                // store a copy then reapply after
+                // displaying error
                 var display = $scope.display;
                 $scope.display = ' er';
+
                 $timeout(function(){
-                    $scope.display = display;
+                    if (gameState.strictMode){
+                        $scope.buttonTap.start();
+                    } else {
+                        $scope.display = display;
+                        gameState.userRecord = [];
+                        playPattern(0);
+                    }
                 }, 1000);
-                userRecord = [];
-                allowClick = false;
-                playPattern(0);
             }
+
+        }
+
+        function restartGame(){
+            $scope.display = 1;
+            gameState.pattern = [];
+            gameState.userRecord = [];
         }
 
 
         function generatePattern(){
             var rand = Math.floor(Math.random() * 4);
-            pattern.push(rand);
+            gameState.pattern.push(rand);
         }
 
         // accepts an array
         function playPattern(index){
 
+            gameState.allowClick = false;
+
             var timer = index === 0 ? 1500 : getTimerValue(index);
 
-            if (index < pattern.length){
-                $timeout(function(){
-                    toggleClass(pattern[index]);
+            if (index < gameState.pattern.length){
+                gameState.timeout.playPattern = $timeout(function(){
+                    toggleClass(gameState.pattern[index]);
                     playPattern(index+1);
+                    gameState.allowClick = index === gameState.pattern.length-1 ? true : false;
                 }, timer);
             }
+        }
 
-            allowClick = true;
+        function clearTimer(){
+            angular.forEach(gameState.timeout, function(time){
+                $timeout.cancel(time);
+            })
         }
 
         function getTimerValue(){
 
-            if (pattern.length < 3){
-                return 1500;
-            } else if (pattern.length < 6){
+            if (gameState.pattern.length < 3){
                 return 1000;
-            } else if (pattern.length < 9){
-                return 700;
+            } else if (gameState.pattern.length < 6){
+                return 800;
+            } else if (gameState.pattern.length < 9){
+                return 600;
             } else {
                 return 400;
             }
